9668f71d39c23afad38c02244f234d731139c212
privateseb <sebastian.vasser@gmail.com>
2022-06-29 21:50:55 +0200

    Implement image combining into pairs

--- src/card-db/pair-images.ts
+++ src/card-db/pair-images.ts
@@ -1,5 +1,7 @@
 import fs from 'fs'
 import { Request, Response } from 'express'
+import images from 'images'
+import uniqid from 'uniqid'
 
 export function pairImagesView(_req: Request, res: Response) {
   const images = fs.readdirSync('png')
@@ -7,6 +9,9 @@ export function pairImagesView(_req: Request, res: Response) {
 }
 
 export function pairImages(req: Request, res: Response) {
-  console.log(req.body)
+  images(2048, 2048)
+    .draw(images(`./png/${req.body.front}`), 0, 0)
+    .draw(images(`./png/${req.body.back}`), 1024, 0)
+    .save(`./pairs/${uniqid()}.png`, { quality: 100 })
   res.redirect("/pair-images")
 }
