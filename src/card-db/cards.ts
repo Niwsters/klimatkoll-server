fb77de1f6d874e88121deaf8f529921a049a50e7
Niwsters <sebastian.vasser@gmail.com>
2022-07-04 15:13:22 +0200

    Refactor card database to event sourced

--- src/card-db/cards.ts
+++ src/card-db/cards.ts
@@ -7,10 +7,29 @@ type Card = {
   image: string
 }
 
+type EventRow = {
+  event: string
+}
+
+type Event = {
+  type: string,
+  payload: any
+}
+
 function database() {
   const db = new Database(':memory:')
-  db.exec("CREATE TABLE IF NOT EXISTS Cards (name TEXT UNIQUE, image TEXT UNIQUE, emissions REAL)")
+  db.exec(`CREATE TABLE IF NOT EXISTS Events (
+    id INTEGER NOT NULL PRIMARY KEY,
+    timestamp TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    aggregate TEXT NOT NULL,
+    event TEXT NOT NULL
+  )`)
   return db
+}
+
+async function events(db: Database, aggregate: string): Promise<Event[]> {
+  const rows = await query<EventRow[]>(db, "SELECT event FROM Events WHERE aggregate LIKE ?", [aggregate])
+  return rows.map(row => JSON.parse(row.event))
 }
 
 export function ensureCardDBCreated(): Database {
@@ -37,24 +56,55 @@ async function exec(db: Database, sql: string, params: any[] = []) {
   })
 }
 
+function card_created(cards: Card[], event: Event): Card[] {
+  return [
+    ...cards,
+    {
+      name: event.payload.name,
+      image: event.payload.image
+    }
+  ]
+}
+
+type Handler = (cards: Card[], event: Event) => Card[]
+
+const handlers: { [eventType: string]: Handler } = {
+  "card_created": card_created
+}
+
+function onEvent(cards: Card[], event: Event): Card[] {
+  const handler = handlers[event.type]
+  if (handler) return handler(cards, event)
+  return cards
+}
+
 export async function cards(db: Database): Promise<Card[]> {
-  return await query<Card[]>(db, "SELECT name, image FROM Cards")
+  const cardEvents = await events(db, "card")
+  return cardEvents.reduce(onEvent, [])
 }
 
 export async function card(db: Database, name: string): Promise<Card> {
-  const result = await query<Card[]>(db, "SELECT name, image FROM Cards WHERE name = ?", [name])
-  return result[0]
+  const result = (await cards(db)).find(card => card.name === name)
+  if (!result) throw new Error(`Could not find card with name: ${name}`)
+  return result
 }
 
-export async function createCard(db: Database, src: string): Promise<Card> {
-  const name = src.replace(/.png$/, '')
-  await exec(db, "INSERT INTO Cards (name, image) VALUES (?, ?)", [name, '/' + src])
-  return card(db, src)
+export async function createCard(db: Database, image: string): Promise<Card> {
+  const name = image.replace(/.png$/, '')
+  const event = {
+    type: "card_created",
+    payload: {
+      name,
+      image: '/' + image
+    }
+  }
+  await exec(db, "INSERT INTO Events (aggregate, event) VALUES (?, ?)", ["card", JSON.stringify(event)])
+  return card(db, name)
 }
 
 // Loops through existing image pairs and adds cards into database if missing
 export async function processImagePairs(db: Database) {
-  const existing = (await query<Card[]>(db, "SELECT image FROM Cards")).map(card => card.image.replace(/^\//, ''))
+  const existing = (await cards(db)).map(card => card.image.replace(/^\//, ''))
   const pairs = fs.readdirSync("./pairs")
   pairs
     .filter(pair => !existing.includes(pair))
