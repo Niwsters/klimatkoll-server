6a6274814e675637be21b475eb84cf59e11f0895
privateseb <sebastian.vasser@gmail.com>
2022-09-02 01:18:58 +0200

    Change card forms to a single form

--- src/card-db/card-view.ts
+++ src/card-db/card-view.ts
@@ -17,27 +17,6 @@ function listView(db: Database): Controller {
   return async (_req, res) => res.render("cards", { cards: await cards(db), languages: languages(await events(db, "language")) })
 }
 
-function setNameView(db: Database): Controller {
-  return async (req, res) => {
-    setCardName(db, req.body.id, req.body.name)
-    res.redirect("/admin/cards")
-  }
-}
-
-function setEmissionsView(db: Database): Controller {
-  return async (req, res) => {
-    setCardEmissions(db, req.body.id, req.body.emissions)
-    res.redirect("/admin/cards")
-  }
-}
-
-function setLanguageView(db: Database): Controller {
-  return async (req, res) => {
-    setCardLanguage(db, req.body.id, req.body.language)
-    res.redirect("/admin/cards")
-  }
-}
-
 function listJSONView(db: Database): Controller {
   return async (_req, res) => {
     res.json(await cards(db))
@@ -52,11 +31,18 @@ function removeView(db: Database, location: Location): Controller {
   }
 }
 
+function updateView(db: Database): Controller {
+  return async (req, res) => {
+    setCardName(db, req.body.id, req.body.name)
+    setCardEmissions(db, req.body.id, req.body.emissions)
+    setCardLanguage(db, req.body.id, req.body.language)
+    return res.redirect('/admin/cards')
+  }
+}
+
 export default {
   list: listView,
-  setEmissions: setEmissionsView,
-  setName: setNameView,
-  setLanguage: setLanguageView,
   listJSON: listJSONView,
-  remove: removeView
+  remove: removeView,
+  update: updateView
 }
