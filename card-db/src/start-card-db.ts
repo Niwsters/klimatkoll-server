17d177527a8a67df331dbb1a4d5dc831762c4a83
Niwsters <sebastian.vasser@gmail.com>
2022-10-14 20:56:04 +0200

    Move card DB views folder and clean up

--- card-db/src/start-card-db.ts
+++ card-db/src/start-card-db.ts
@@ -1,8 +1,8 @@
 import http from 'http'
 import express from 'express'
 import fileUpload from 'express-fileupload'
+import path from 'path'
 import { routes } from './routes'
-import { spawn } from 'child_process'
 import bodyParser from 'body-parser'
 import { ensureEventsDBCreated } from './events'
 import { auth } from './auth'
@@ -10,29 +10,12 @@ import { startCardImageProcessing } from './start-card-image-processing'
 import { location } from './location'
 import fs from 'fs'
 
-async function isProgramInstalled(program: string): Promise<boolean> {
-  return new Promise(resolve => {
-    const process = spawn(program, ['--version'], { shell: true })
-    process.stdout.on('data', () => resolve(true))
-    process.stderr.on('data', () => resolve(false))
-  })
-}
-
-async function checkRequirements() {
-  for (const program of ['pdf2svg']) {
-    if (!await isProgramInstalled(program))
-      throw new Error(`${program} not installed`)
-  }
-}
-
 function rewriteURL(req: any, _: any, next: any) {
   req.url = req.url.replace(/^\/admin/, "")
   next()
 }
 
 async function app() {
-  await checkRequirements()
-
   const config = JSON.parse(fs.readFileSync("./card-db-config.json").toString())
   const loc = location(config.dataLocation)
   const db = ensureEventsDBCreated(loc.root)
@@ -50,12 +33,14 @@ async function app() {
   e.use(bodyParser.json())
 
   e.set('view engine', 'pug')
+  e.set('views', path.join('./card-db/views'))
+
   routes(db, loc).forEach(route => {
     switch (route.method) {
       case "post":
-	e.post(route.url, route.controller)
+	      return e.post(route.url, route.controller)
       default:
-	e.get(route.url, route.controller)
+	      return e.get(route.url, route.controller)
     }
   })
 
--- run-server.sh
+++
@@ -1,1 +0,0 @@
-PORT=80 npm start > app.log
