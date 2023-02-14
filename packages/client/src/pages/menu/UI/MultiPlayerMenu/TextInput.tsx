8a380bd03dffd6e947d55334f98a34bd1317e3c4
Niwsters <sebastian.vasser@gmail.com>
2023-02-14 21:19:33 +0100

    Fix CSS properties

--- packages/client/src/pages/menu/UI/MultiPlayerMenu/TextInput.tsx
+++ packages/client/src/pages/menu/UI/MultiPlayerMenu/TextInput.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import _React, { CSSProperties } from 'react'
 
 export function TextInput(placeholder: string, onChangeInput: (e: string) => void, appWidth: number) {
   function onChange(event: any) {
     onChangeInput(event.target.value)
   }
 
-  const style: any = {
-    'display': 'block',
-    'border': 'none',
-    'border-radius': 0,
-    'padding': `${0.0052*appWidth}px ${0.0104*appWidth}px`,
-    'width': '100%',
-    'box-sizing': 'border-box',
-    'font-family': "'Poppins', sans-serif",
-    'font-size': "1em",
+  const style: CSSProperties = {
+    display: 'block',
+    border: 'none',
+    borderRadius: 0,
+    padding: `${0.0052*appWidth}px ${0.0104*appWidth}px`,
+    width: '100%',
+    boxSizing: 'border-box',
+    fontFamily: "'Poppins', sans-serif",
+    fontSize: "1em",
   }
 
   return <input type="text" placeholder={placeholder} onChange={onChange} style={style} />
