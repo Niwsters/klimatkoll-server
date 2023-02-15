0782f9caae87eed17231448863f67577e029638b
Niwsters <sebastian.vasser@gmail.com>
2023-02-15 19:49:29 +0100

    Implement showing new cards on screen in single player

--- packages/client/src/app/Router.tsx
+++ packages/client/src/app/Router.tsx
@@ -3,11 +3,26 @@ import { MultiPlayerServer } from '../socket/multiplayer-server'
 import { EventToAdd } from '../event/event'
 import { useState } from 'react'
 import { Menu } from '../pages/menu/UI/Menu'
-import * as Canvas from '../components/Canvas'
+import { BasicGame } from '../components/BasicGame'
 import { TFunction } from 'i18next'
+import * as Board from '../core2/board'
+import * as Card from '../core2/card'
 import * as SampleCards from '../stories/sample_cards'
 
 export type Page = "menu" | "canvas"
+
+const positioning: Card.CardPositioning = {
+  x: 0,
+  y: 0,
+  rotation: 0,
+  scale: 1,
+  zLevel: 0
+}
+
+const card = Card.create(SampleCards.card.name, positioning)
+const card2 = Card.create(SampleCards.card2.name, positioning)
+const card3 = Card.create(SampleCards.card3.name, positioning)
+const deck = [card, card2, card3]
 
 export function Router(props: { mpServer: MultiPlayerServer, root: Root, t: TFunction }) {
   const { mpServer, root, t } = props
@@ -22,7 +37,7 @@ export function Router(props: { mpServer: MultiPlayerServer, root: Root, t: TFun
   }
 
   const [page, setPage] = useState<Page>("menu")
-  const onEvent = (event: EventToAdd) => {
+  const onMenuEvent = (event: EventToAdd) => {
     setPage(route(event))
   }
 
@@ -30,15 +45,25 @@ export function Router(props: { mpServer: MultiPlayerServer, root: Root, t: TFun
     httpServerURL={root.environment.httpServerURL}
     resolution$={root.resolution$}
     mpServer={mpServer.inbox}
-    addEvent={onEvent}
+    addEvent={onMenuEvent}
     t={t}
     />
 
-  const getCards: Canvas.GetCards = () => []
-  const getCardDesign: Canvas.GetCardDesign = (_name: string) => SampleCards.card
-  const canvas = <Canvas.Component
-    getCards={getCards}
-    getCardDesign={getCardDesign}
+  const onGameEvent = (event: EventToAdd) => console.log(event)
+
+  const currentTime = Date.now()
+  
+  let board = Board.create([...deck, ...deck])
+  board = Board.drawHandCard(board, currentTime)
+  board = Board.drawHandCard(board, currentTime)
+  board = Board.drawHandCard(board, currentTime)
+
+  board = Board.playCardFromDeck(board, currentTime)
+  board = Board.playCardFromDeck(board, currentTime)
+  board = Board.playCardFromDeck(board, currentTime)
+  const singleplayer = <BasicGame
+    board={board}
+    onEvent={onGameEvent}
     />
 
   const component = (route: Page) => {
@@ -46,7 +71,7 @@ export function Router(props: { mpServer: MultiPlayerServer, root: Root, t: TFun
       case "menu":
         return menu
       case "canvas":
-        return canvas
+        return singleplayer
     }
   }
 
